ed4a8b804af1a14a2fcf99a68c5834d8c760d8bd
invainX <54872879+invainX@users.noreply.github.com>
2020-09-16 21:30:11 +0800

    Add files via upload

---
+++ CosineSumilrity.py
@@ -0,0 +1,50 @@
+class CosineSimilarity(object):
+    def __init__(self, content_x1, content_y2):
+        self.s1 = content_x1
+        self.s2 = content_y2
+
+    @staticmethod
+    def extract_keyword(content):  # 提取关键词
+        # 正则过滤 html 标签
+        re_exp = re.compile(r'(<style>.*?</style>)|(<[^>]+>)', re.S)
+        content = re_exp.sub(' ', content)
+        # html 转义符实体化
+        content = html.unescape(content)
+        # 切割
+        seg = [i for i in jieba.cut(content, cut_all=True) if i != '']
+        # 提取关键词
+        keywords = jieba.analyse.extract_tags("|".join(seg), topK=200, withWeight=False)
+        return keywords
+
+    @staticmethod
+    def one_hot(word_dict, keywords):  # oneHot编码
+        # cut_code = [word_dict[word] for word in keywords]
+        cut_code = [0]*len(word_dict)
+        for word in keywords:
+            cut_code[word_dict[word]] += 1
+        return cut_code
+
+    def main(self):
+        # 提取关键词
+        keywords1 = self.extract_keyword(self.s1)
+        keywords2 = self.extract_keyword(self.s2)
+        # 词的并集
+        union = set(keywords1).union(set(keywords2))
+        # 编码
+        word_dict = {}
+        i = 0
+        for word in union:
+            word_dict[word] = i
+            i += 1
+        # oneHot编码
+        s1_cut_code = self.one_hot(word_dict, keywords1)
+        s2_cut_code = self.one_hot(word_dict, keywords2)
+        # 余弦相似度计算
+        sample = [s1_cut_code, s2_cut_code]
+        # 除零处理
+        try:
+            sim = cosine_similarity(sample)
+            return sim[1][0]
+        except Exception as e:
+            print(e)
+            return 0.0
